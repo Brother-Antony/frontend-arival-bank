4e0d6c578c1cc8fd60c7b63d7e789bc547cf54d0
brother-antony <elmer.antony12@gmail.com>
2023-10-25 20:45:34 -0500

    err

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { useRouteError, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound: React.FC = () => {
-  const error = useRouteError()
   const navigate = useNavigate()
 
   return (
@@ -10,7 +9,7 @@ const NotFound: React.FC = () => {
       <i className='fad fa-outlet text-[54px] text-[var(--bg-current2)] mr-[1rem]'></i>
       <i className='fad fa-plug text-[54px] text-[var(--bg-current2)]'></i>
       <div className='text-[var(--label)] text-8xl font-bold my-4'>404</div>
-      <div className='text-[var(--label)] font-bold text-2xl'>{error.error.message}</div>
+      <div className='text-[var(--label)] font-bold text-2xl'>No routes matched location</div>
       <div className='mt-6 text-[var(--title)] text-base'>The page you are looking for was not found. <br /> Check the URL or return to the <button type='button' onClick={() => navigate(-1)} className='underline text-[var(--bg-current2)] font-bold'>Homepage.</button></div>
     </section>
   )
